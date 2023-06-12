30d704fe540432e72035511d7cb0efb98e2a237a
MSinghSolanki <112701441+MSinghSolanki@users.noreply.github.com>
2023-06-12 16:34:39 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,65 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const cors = require('cors');
+const connect = require("./configs/db");
+const app = express();
+const userController = require("./controllers/user.controller");
+const productController = require("./controllers/product.controller");
+const { register, login, newToken } = require("./controllers/auth.controlller")
+
+const passport = require("./configs/google.oauth");
+
+// const fileupload = require("express-fileupload")
+
+
+// app.use(fileupload({
+//   useTempFiles:true
+// }))
+
+
+
+app.use(cors());
+
+app.use(express.json());
+
+// /register
+app.post("/register", register);
+// .login
+app.post("/login", login);
+
+app.use("/users", userController);
+app.use("/products", productController);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+  }),
+  (req, res) => {
+    const { user } = req;
+    const token = newToken(user);
+
+    return res.send({ user, token });
+  }
+);
+
+app.listen(2754, async () => {
+  try {
+    await connect();
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log("listening on port 2754");
+});
